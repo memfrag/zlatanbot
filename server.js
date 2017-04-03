466eb46d2b20064588119592e518e0098e1eb3a7
Martin Johannesson <mjohannesson@gmail.com>
2017-04-04 01:10:14 +0200

    Fixed import problem regarding allsvenskan.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 const pad = require('pad');
-const allsvenskan = require('allsvenskan.js')
+const allsvenskan = require('./allsvenskan.js')
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
