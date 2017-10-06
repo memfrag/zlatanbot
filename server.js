53be3b98577b3651bdfcef9d2c2e65f6c45a1f8a
Martin Johannesson <martin.johannesson@bontouch.com>
2017-10-06 21:03:59 +0200

    maybe this time.

--- server.js
+++ server.js
@@ -626,7 +626,7 @@ slapp.message('fpl', ['mention', 'direct_message'], (msg) => {
 slapp.message('nhl', ['mention', 'direct_message'], (msg) => {
 	msg.say("Fetching NHL stats...");
 	nhl.updateNHLStandings(function (standings) {
-		msg.say("NHL STANDINGS!!!");
+		msg.say("TEST NHL STANDINGS!!!");
         msg.say(standings);
     });
 })
